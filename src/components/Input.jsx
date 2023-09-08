e5e0bd268ce2d19d1f12ec2a347406252e5cfe90
AndresRodriguez-98 <andresrod_98@hotmail.com>
2023-09-08 00:28:21 -0300

    WIP usando lib para form validations

---
+++ src/components/Input.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useFormContext } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
+const Input = ({ name, type, placeholder, registerValidators }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <div>
+      <input
+        className="bg-gray-200 py-2 px-10 text-gray-400 rounded-lg outline-none"
+        type={type}
+        placeholder={placeholder}
+        {...register(name, registerValidators)}
+      />
+      <ErrorMessage name={name} errors={errors} />
+    </div>
+  );
+};
+
+export default Input;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,45 +1,37 @@
 import React from "react";
-import swal from 'sweetalert'
+import { useForm, FormProvider } from "react-hook-form";
+import Input from "./Input";
 
 const Register = () => {
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const name = e.target.name.value;
-    const phone = e.target.phone.value;
-    const email = e.target.email.value;
-    const password = e.target.password.value;
+  const methods = useForm();
 
-    function validateEmail(email) {
-      var re = /\S+@\S+\.\S+/;
-      return re.test(email);
-    }
-
-    function validatePhone(phone) {
-      var re =
-        /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/;
-      return re.test(phone);
-    }
-
-    if (name === "" || phone === "" || email === "" || password === "") {
-      swal(<h2>Los campos no pueden estar vacios</h2>);
-      return;
-    }
-
-    if (email !== "" && !validateEmail(email)) {
-      swal(<h2>Debes escribir una dirección de correo válida</h2>);
-      return;
-    }
-
-    if (phone !== "" && !validatePhone(phone)) {
-      swal(<h2>Debes escribir un número de teléfono válido</h2>);
-      return;
-    }
-
-    if (name !== "Andrés Rodríguez" || phone !== "02214345359" || email !== "reloo.cod@gmail.com" || password !== "react") {
-      swal(<h2>Credenciales inválidas</h2>);
-      return;
-    }
+  const nameValidator = {
+    required: "Debe ingresar un nombre completo",
+    maxLength: 40,
   };
+  const phoneValidator = {
+    required: "Debe ingresar un teléfono",
+    pattern: {
+      value:
+        /^(?:(?:00)?\+?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/i,
+      message: "El número de teléfono no es válido",
+    },
+  };
+  const emailValidator = {
+    required: "Debe ingresar una dirección de email",
+    pattern: {
+      value: /\S+@\S+\.\S+/i,
+      message: "La dirección de email no es válida",
+    },
+  };
+  const passwordValidator = {
+    required: "Debe ingresar una contraseña",
+    maxLength: 50,
+  };
+
+  const onSubmit = methods.handleSubmit((data) => {
+    console.log(data);
+  });
 
   return (
     <div className="flex flex-col p-0 gap-32 items-start absolute w-[352px] h-[72px] left-[121px] bottom-[703px]">
@@ -53,63 +45,56 @@ const Register = () => {
         </p>
       </div>
 
-      <form
-        onSubmit={submitHandler}
-        className="flex flex-col items-start px-0 py-0 p-[197px] gap-6 absolute w-[352px] h-[586px] left-12 top-64"
-      >
-        <div className="flex flex-row items-center p-0 gap-[9px] w-52 h-12">
-          <img src="" alt="" className="w-12 h-12" />
-          <a href="">Subí tu foto de perfil</a>
-        </div>
-
-        <label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Nombre y Apellido"
-            className="bg-gray-200 py-2 px-10 text-gray-400 rounded-lg outline-none"
-          />
-        </label>
-
-        <label>
-          <input
-            type="text"
-            name="tel"
-            placeholder="+54 01 0200 000"
-            className="bg-gray-200 py-2 px-10 text-gray-400 rounded-lg outline-none"
-          />
-        </label>
-
-        <label>
-          <input
-            type="text"
-            name="email"
-            placeholder="hola@tuemail.com"
-            className="bg-gray-200 py-2 px-10 text-gray-400 rounded-lg outline-none"
-          />
-        </label>
-
-        <label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Ingresá tu contraseña"
-            className="bg-gray-200 py-2 px-10 text-gray-400 rounded-lg outline-none"
-          />
-        </label>
-
-        <button
-          type="submit"
-          className="flex flex-row justify-center items-center px-3 py-6 gap-[10px] w-[327px] h-[48px] bg-gradient-to-r from-[#6941C6] to-[#7065F0] rounded-lg text-white not-italic font-bold text-base text-center"
+      <FormProvider {...methods}>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="flex flex-col items-start px-0 py-0 p-[197px] gap-6 absolute w-[352px] h-[586px] left-12 top-64"
+          noValidate
+          autoComplete="off"
         >
-          Registrate
-        </button>
+          <div className="flex flex-row items-center p-0 gap-[9px] w-52 h-12">
+            <img src="" alt="" className="w-12 h-12" />
+            <a href="">Subí tu foto de perfil</a>
+          </div>
 
-      </form>
+          <Input
+            name="nombre"
+            type="text"
+            placeholder="Nombre y Apellido"
+            registerConfig={nameValidator}
+          />
+
+          <Input
+            name="phone"
+            type="text"
+            placeholder="+54 01 0200 000"
+            registerConfig={phoneValidator}
+          />
+
+          <Input
+            name="email"
+            type="text"
+            placeholder="hola@tuemail.com"
+            registerConfig={emailValidator}
+          />
+
+          <Input
+            name="password"
+            type="password"
+            placeholder="Ingresá tu contraseña"
+            registerConfig={passwordValidator}
+          />
+
+          <button
+            onClick={onSubmit}
+            className="flex flex-row justify-center items-center px-3 py-6 gap-[10px] w-[327px] h-[48px] bg-gradient-to-r from-[#6941C6] to-[#7065F0] rounded-lg text-white not-italic font-bold text-base text-center"
+          >
+            Registrate
+          </button>
+        </form>
+      </FormProvider>
 
       <div className="flex flex-col items-center p-0 gap-5 absolute w-80 h-20 left-6 top-[633px]">
-        
-
         <div className="flex-row flex gap-2">
           <p className="text-gray-400">¿Ya tenés una cuenta?</p>
           <a href="" className="text-black">
